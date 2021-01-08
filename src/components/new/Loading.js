3ca737a62e4ec67d31d84c5dfa0c4559fb7d7386
JoshAnderson123 <joshfanderson1@gmail.com>
2021-01-08 15:43:31 +0000

    Added site icon

--- src/components/new/Loading.js
+++ src/components/new/Loading.js
@@ -5,6 +5,7 @@ export default function Loading() {
   return (
     <div className="loading-container stretch f-c">
       <img className="loading-image" src={icon} alt="Loading" />
+      <div className="font-medium" style={{marginTop: "10px"}}>Loading data</div>
     </div>
   )
 }
