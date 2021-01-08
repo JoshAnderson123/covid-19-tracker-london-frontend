045c39834ae51e4a877f6265f6b210408adfe104
JoshAnderson123 <joshfanderson1@gmail.com>
2021-01-08 16:04:56 +0000

    Removed chakra error

--- src/components/new/SideBar.js
+++ src/components/new/SideBar.js
@@ -5,7 +5,7 @@ import { Box, Image, AspectRatio } from "@chakra-ui/react"
 
 export default function SideBar() {
   return (
-    <div className="sb-container f-vs" justify="space-around" align="center" divGrow="0" bgColor="#fff">
+    <div className="sb-container f-vs">
       <AspectRatio className="sb-logo" ratio={1} flexShrink="0">
         <Image className="stretch" objectFit="contain" src={icon} alt="virus icon" />
       </AspectRatio>
@@ -15,8 +15,6 @@ export default function SideBar() {
         <Box>Design Engineering</Box>
         <Box>Sensing and IoT</Box>
       </Box>
-      {/* <Box className="font-medium-small sidebar-text ta-center" style={{marginTop: "40px"}}>Last Updated:</Box>
-      <Box className="font-medium-small sidebar-text ta-center">Jan 3rd, 2020</Box> */}
     </div>
   )
 }
