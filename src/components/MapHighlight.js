ede2d77ade728671e6ea78ed19ede5b9235aadb2
JoshAnderson123 <joshfanderson1@gmail.com>
2021-01-11 20:43:40 +0000

    Fixed highlight bug

--- src/components/MapHighlight.js
+++ src/components/MapHighlight.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../css/LondonMap.css'
 import { pathData } from '../config'
+import { trimArea } from '../util'
 
 export default function MapHighlight({ area, type }) {
+
+  const doubleArea = ["Hackney and City of London", "Barking and Dagenham"].includes(trimArea(area)) ? `${trimArea(area)} 2` : false
 
   return (
     <svg
@@ -12,7 +15,8 @@ export default function MapHighlight({ area, type }) {
       xlink="http://www.w3.org/1999/xlink"
       x="0px" y="0px" viewBox="0 0 756 606.7"
     >
-      <path d={pathData[area]} />
+      <path d={pathData[trimArea(area)]} />
+      {doubleArea ? <path d={pathData[doubleArea]} /> : null}
     </svg>
   )
 }
